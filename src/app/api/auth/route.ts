9468be900cbcad9770c75c4c23cd05326300f6d6
Manavk2004 <mjkamdar04@gmail.com>
2025-11-29 22:22:12 -0500

    Nylas implemented

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,27 @@
+import { redirect } from 'next/navigation'
+import Nylas from 'nylas'
+import React from 'react'
+
+const NylasConfig = {
+    apiKey: process.env.NYLAS_API_KEY as string,
+    apiUri: process.env.NYLAS_API_URI as string,
+}
+
+const nylas = new Nylas(NylasConfig)
+
+const AuthConfig = {
+    clientId: process.env.NYLAS_CLIENT_ID as string,
+    redirectUri: "http://localhost:3000/oauth/exchange"
+}
+
+export async function GET(request: Request) {
+    console.log("Inside get")
+    const authUrl = nylas.auth.urlForOAuth2({
+        clientId: AuthConfig.clientId as string,
+        redirectUri: AuthConfig.redirectUri as string,
+    })
+
+    return redirect(authUrl)
+}
+
+export default GET
